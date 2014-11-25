3c77e3e866588ba8323a418a3ca4bf2aaa32daf1
Florent Jaby <fjaby@octo.com>
2014-11-25 15:40:36 +0100

    add method toJSON

--- test/200-timer.js
+++ test/200-timer.js
@@ -35,6 +35,46 @@ describe('timer', function () {
         expect(timer.stop('myKey')).to.equal(400);
       });
     });
+
+    describe('with a later call to .start(otherKey)', function () {
+      beforeEach(function () {
+        timer.start('myKey');
+        clock.tick(200);
+        timer.start('myOtherKey');
+        clock.tick(100);
+      });
+      describe('.stop(key)', function () {
+        it('return the time elapsed since the call to start with the same key', function () {
+          expect(timer.stop('myKey')).to.equal(300);
+        });
+      });
+
+      describe('.stop(otherKey)', function () {
+        it('returns the time since the call to start with otherKey', function () {
+          expect(timer.stop('myOtherKey')).to.equal(100);
+        })
+      })
+    });
+
+    describe('.toJSON()', function () {
+      beforeEach(function () {
+        timer.start('keyA');
+        clock.tick(100);
+        timer.start('keyB');
+        timer.start('keyC');
+        clock.tick(500);
+        timer.start('keyD');
+        clock.tick(1000);
+      });
+      it('returns an object with all the durations since each call to start', function () {
+        expect(timer.toJSON()).to.deep.equal({
+          keyA: 1600,
+          keyB: 1500,
+          keyC: 1500,
+          keyD: 1000
+        });
+      });
+    });
   });
 
   describe('.stop(key)', function () {
