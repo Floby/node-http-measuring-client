e0258ea975ccebdc628237f37a34f33374ea7011
Florent Jaby <fjaby@octo.com>
2014-11-27 14:43:50 +0100

    make real requests for tests

---
+++ test/300-real-requests.js
@@ -0,0 +1,39 @@
+var url = require('url');
+var sinon = require('sinon');
+var blackhole = require('stream-blackhole');
+var expect = require('chai').expect;
+
+describe('When making actual requests', function () {
+  var mhttp, server;
+  
+  beforeEach(function (done) {
+    mhttp = require('../').create();
+    server = mhttp.createServer(function (req, res) {
+      setTimeout(res.end.bind(res, 'Hello World\n'), 500);
+    }).listen(0, function (err) {
+      port = server.address().port;
+      done(err)
+    });
+  });
+  afterEach(function (done) {
+    server.close(done);
+  });
+
+
+  it('measure the totalTime', function (done) {
+    var onStat = sinon.spy(assertions);
+    mhttp.on('stat', onStat);
+
+    mhttp.request('http://localhost:'+port+'/hello', function (response) {
+      response.pipe(blackhole()).on('finish', function () {
+        expect(onStat.calledOnce).to.equal(true, 'onStat was not called');
+      });
+    }).end();
+
+    function assertions (uri, stats) {
+      expect(url.format(uri)).to.equal('http://localhost:'+port+'/hello');
+      expect(stats.totalTime).to.be.within(500, 520);
+      done();
+    }
+  });
+});
